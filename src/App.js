fd7a1f8bab93b8ef307c8fa4e2dd59b33c60e565
Dani <koskas.dani@gmail.com>
2021-10-29 15:09:48 +0300

    in progress

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 import { useState } from "react";
 import "./App.css";
+import SingleCard from "./components/SingleCard";
 
 const cardsImages = [
-  { "src": "/public/helmet-1.png" },
-  { "src": "/public/shield-1.png" },
-  { "src": "/public/ring-1.png" },
-  { "src": "/public/potion-1.png" },
-  {"src":"/public/sword-1.png"},
-  {"src":"/public/scroll-1.png"},
+  { "src": "/img/helmet-1.png" },
+  { "src": "/img/shield-1.png" },
+  { "src": "/img/ring-1.png" },
+  { "src": "/img/potion-1.png" },
+  {"src":"/img/sword-1.png"},
+  {"src":"/img/scroll-1.png"},
 ];
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  
   
   // shuffle cards
-  const shuffleCards = () =>{ 
+  const shuffleCards = () => { 
     const shuffledCards = [...cardsImages, ...cardsImages]
     .sort(()=> Math.random()-0.5)
     .map((card)=> ({ ...card, id:Math.random() }))
-    setCards(shuffleCards)
+    setCards(shuffledCards)
     setTurns(0)
 
   }
-  console.log(cards, turns)
+
+  // handle a choice
+  const handleChoice = (card) =>{
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
   
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map((card)=>(
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice}/>
+        ))}
+      </div>
     </div>
   );
 }
